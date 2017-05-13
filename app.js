8a8b2767f6eb14730c7190945e2ccff880c28c13
Florian Tönjes <skatepainter@yahoo.de>
2017-05-13 19:40:24 +0200

    Format

--- app.js
+++ app.js
@@ -15,15 +15,12 @@ app.use('/static', express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
 
-
 app.get("/", function (req, res, next) {
     res.render("home");
 });
 
 app.use("/all_books", books);
-
 app.use("/all_loans", loans);
-
 app.use("/all_patrons", patrons);
 
 sequelize.sync().then(function () {
