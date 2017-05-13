82689a71a7f99dcbc7decebc8c5f2213c74ced82
Florian Tönjes <skatepainter@yahoo.de>
2017-05-13 17:23:51 +0200

    Implement all_patrons

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const Book = require("./models").books;
+const Patron = require("./models").patrons;
 const sequelize = require("./models").sequelize;
 
 const app = express();
@@ -17,14 +18,24 @@ app.get("/", function (req, res, next) {
 });
 
 app.get("/all_books", function (req, res, next) {
-    Book.findAll().then(function(books) {
-        res.render("all_books", {books: books});
+    Book.findAll().then(function (books) {
+        res.render("all_books", {
+            books: books
+        });
+    });
+});
+
+app.get("/all_patrons", function (req, res, next) {
+    Patron.findAll().then(function (patrons) {
+        res.render("all_patrons", {
+            patrons: patrons
+        });
     });
 });
 
 
 sequelize.sync().then(function () {
-  app.listen(3000, function () {
-    console.log("The frontend server is running on port 3000!");
-  });
+    app.listen(3000, function () {
+        console.log("The frontend server is running on port 3000!");
+    });
 });
