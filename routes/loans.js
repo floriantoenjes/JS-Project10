3c498a3e3fcd4ef3a6879df29229a0371b184ad3
Florian Tönjes <skatepainter@yahoo.de>
2017-05-13 20:36:37 +0200

    Add include to loans.js routing

--- routes/loans.js
+++ routes/loans.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const Book = require("../db.js").books;
 const Loan = require("../db.js").loans;
+const Patron = require("../db.js").patrons;
 
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-    Loan.findAll().then(function (loans) {
-        res.render("all_loans", {
-            loans: loans
-        });
+    Loan.findAll({
+        include: [
+            {
+                model: Book
+                },
+            {
+                model: Patron
+            }
+            ]
+    }).then(function (results) {
+        res.send("Hello World!");
     });
 });
 
