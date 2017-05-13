547a6902bf18862fd2d8c4cc70f06b13a4292113
Florian Tönjes <skatepainter@yahoo.de>
2017-05-13 20:43:27 +0200

    Start constructing loan object

--- routes/loans.js
+++ routes/loans.js
@@ -16,7 +16,22 @@ router.get("/", function (req, res, next) {
             }
             ]
     }).then(function (results) {
-        res.send("Hello World!");
+        const loanes = [];
+        for (let result of results) {
+            console.log("Result");
+            const loan = {
+                book: result.book.title,
+                patron_first_name: result.patron.first_name,
+                patron_last_name: result.patron.last_name,
+                loaned_on: result.loaned_on,
+                return_by: result.return_by,
+                returned_on: result.returned_on
+            };
+            loanes.push(loan);
+        }
+//        res.render("all_loans", {loanes: loanes});
+        console.log(loanes.length);
+        res.send("Hello");
     });
 });
 
