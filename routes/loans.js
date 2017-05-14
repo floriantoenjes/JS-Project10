01231bbc00b30542e15cbd0e01c0f2d17a84fe11
Florian Tönjes <skatepainter@yahoo.de>
2017-05-14 09:23:27 +0200

    Finish implement overdue and checked_out loans

--- routes/loans.js
+++ routes/loans.js
@@ -8,8 +8,48 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
     if (req.query.filter === "overdue") {
 
+        Loan.findAll({
+            where: {
+                returned_on: null,
+                return_by: {
+                    $lt: Date()
+                }
+            },
+            include: [
+                {
+                    model: Book
+                },
+                {
+                    model: Patron
+            }
+            ]
+        }).then(function (results) {
+            const loans = [];
+            res.render("loans", {
+                loans: results
+            });
+        });
+
     } else if (req.query.filter === "checked_out") {
 
+        Loan.findAll({
+            where: {
+                returned_on: null
+            },
+            include: [
+                {
+                    model: Book
+                },
+                {
+                    model: Patron
+            }
+            ]
+        }).then(function (results) {
+            const loans = [];
+            res.render("loans", {
+                loans: results
+            });
+        });
     } else {
 
         Loan.findAll({
