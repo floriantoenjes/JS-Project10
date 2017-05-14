5cacb189194263a710acc81b047d1179236ec159
Florian Tönjes <skatepainter@yahoo.de>
2017-05-14 19:47:52 +0200

    Add validation for first and last name

--- models/patrons.js
+++ models/patrons.js
@@ -1,24 +1,38 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var patrons = sequelize.define('patrons', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    address: DataTypes.STRING,
-    email: DataTypes.STRING,
-    library_id: DataTypes.STRING,
-    zip_code: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    },
-      timestamps: false,
-      underscored: true
-  });
-  return patrons;
+module.exports = function (sequelize, DataTypes) {
+    var patrons = sequelize.define('patrons', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "First name is required"
+                }
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Last name is required"
+                }
+            }
+        },
+        address: DataTypes.STRING,
+        email: DataTypes.STRING,
+        library_id: DataTypes.STRING,
+        zip_code: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            }
+        },
+        timestamps: false,
+        underscored: true
+    });
+    return patrons;
 };
