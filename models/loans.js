3981e8b790f0d104db4f6dd99f5b7870e811b68d
Florian Tönjes <skatepainter@yahoo.de>
2017-05-14 20:12:35 +0200

    Implement validation for loans model

--- models/loans.js
+++ models/loans.js
@@ -1,23 +1,51 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var loans = sequelize.define('loans', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    book_id: DataTypes.INTEGER,
-    patron_id: DataTypes.INTEGER,
-    loaned_on: DataTypes.DATE,
-    return_by: DataTypes.DATE,
-    returned_on: DataTypes.DATE
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    },
-      timestamps: false,
-      underscored: true
-  });
-  return loans;
+module.exports = function (sequelize, DataTypes) {
+    var loans = sequelize.define('loans', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        book_id: {
+            type: DataTypes.INTEGER,
+            validate: {
+                isInt: {
+                    msg: "Book Id is required"
+                }
+            }
+        },
+        patron_id: {
+            type: DataTypes.INTEGER,
+            validate: {
+                isInt: {
+                    msg: "Patron Id is required"
+                }
+            }
+        },
+        loaned_on: {
+            type: DataTypes.DATE,
+            validate: {
+                isDate: {
+                    msg: "Date is required"
+                }
+            }
+        },
+        return_by: {
+            type: DataTypes.DATE,
+            validate: {
+                isDate: {
+                    msg: "Date is required"
+                }
+            }
+        },
+        returned_on: DataTypes.DATE
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            }
+        },
+        timestamps: false,
+        underscored: true
+    });
+    return loans;
 };
