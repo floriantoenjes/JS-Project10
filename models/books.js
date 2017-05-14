c0dafd1404b2f804c56910dc4ba6ae9843f46a43
Florian Tönjes <skatepainter@yahoo.de>
2017-05-14 19:12:31 +0200

    Add validation to book model

--- models/books.js
+++ models/books.js
@@ -1,22 +1,43 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var books = sequelize.define('books', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true
-    },
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    first_published: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    },
-      timestamps: false,
-      underscored: true
-  });
-  return books;
+module.exports = function (sequelize, DataTypes) {
+    var books = sequelize.define('books', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Title is required"
+                }
+            }
+        },
+        author: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Author is required"
+                }
+            }
+        },
+         genre: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: {
+                    msg: "Genre is required"
+                }
+            }
+        },
+        first_published: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // associations can be defined here
+            }
+        },
+        timestamps: false,
+        underscored: true
+    });
+    return books;
 };
